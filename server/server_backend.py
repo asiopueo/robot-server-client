e4d3bf1e4a33bfda1ce01bb5f561db20e15a6962
Martin Lippl <martin.lippl>
2019-04-08 16:28:59 +0200

    Modified server backend for parameter usage

--- server/server_backend.py
+++ server/server_backend.py
@@ -2,7 +2,7 @@
 
 import sys
 from socket import *
-from time import ctime          # Import necessary modules
+from time import ctime, sleep
 
 import RPi.GPIO as GPIO
 import pantilt
@@ -45,31 +45,90 @@ while True:
 
     print('...connected from :', addr)     # Print the IP address of the client connected with the server.
 
+    # Main loop
     while True:
         data = ''
         data = tcpCliSock.recv(BUFSIZ).decode()    # Receive and decode data sent from the client.
         # Analyze the command received and control the car accordingly.
         if not data:
             break
+
+
+        ####################
+        # Locomotion control
+        ####################
+
         if data == ctrl_cmd[0]:
             print('Motor moving forward')
             motor.forward()
+        elif data[0:8] == 'forward=':
+            print('data =', data)
+            duration = data[8:]
+            try:
+                duration = int(duration)
+                motor.forward()
+                sleep(duration)
+                motor.stop()
+            except:
+                print('Error speed =', duration)
+
         elif data == ctrl_cmd[1]:
             print('Received backward cmd')
             motor.backward()
+        elif data[0:9] == 'backward=':
+            print("data =", data)
+            duration = data.split('=')[1]
+            try:
+                duration = int(duration)
+                motor.backward()
+                sleep(duration)
+                motor.stop()
+            except:
+                print('ERROR, duration =', duration)
+
         elif data == ctrl_cmd[2]:
             print('Received left cmd')
             motor.left()
+        elif data[0:5] == 'left=':    #Turning Angle
+            print('data =', data)
+            duration = data.split('=')[1]
+            try:
+                duration = int(duration)
+                motor.left()
+                sleep(duration)
+                motor.stop()
+            except:
+                print('Error: duration =', duration)
+
         elif data == ctrl_cmd[3]:
             print('Received right cmd')
             motor.right()
+        elif data[0:6] == 'right=':
+            print('data =', data)
+            duration = data.split('=')[1]
+            try:
+                duration = int(duration)
+                motor.right()
+                sleep(duration)
+                motor.stop()
+            except:
+                print('Error: angle =', duration)
+
+
         elif data == ctrl_cmd[4]:
             print('Received stop cmd')
             motor.stop()
+
+
         elif data == ctrl_cmd[5]:
             print('read cpu temp...')
             temp = cpu_temp.read()
             tcpCliSock.send('[%s] %0.2f' % (ctime(), temp))
+
+
+        ####################
+        # Pan/tilt-control
+        ####################
         elif data == ctrl_cmd[8]:
             print('Received x+ cmd')
             pantilt.move_increase_x()
@@ -85,6 +144,11 @@ while True:
         elif data == ctrl_cmd[12]:
             print('home_x_y')
             pantilt.home_x_y()
+        else:
+            print('Command Error! Cannot recognize command: ', data)
+
+
+        """
         elif data[0:5] == 'speed':     # Change the speed
             print(data)
             numLen = len(data) - len('speed')
@@ -96,34 +160,6 @@ while True:
                 if spd < 24:
                     spd = 24
                 #motor.setSpeed(spd)
-
-        elif data[0:5] == 'turn=':    #Turning Angle
-            print('data =', data)
-            angle = data.split('=')[1]
-            try:
-                angle = int(angle)
-                #car_dir.turn(angle)
-            except:
-                print('Error: angle =', angle)
-
-        elif data[0:8] == 'forward=':
-            print('data =', data)
-            spd = data[8:]
-            try:
-                spd = int(spd)
-                #motor.forward(spd)
-            except:
-                print('Error speed =', spd)
-
-        elif data[0:9] == 'backward=':
-            #print("data =", data)
-            spd = data.split('=')[1]
-            try:
-                spd = int(spd)
-                #motor.backward(spd)
-            except:
-                print('ERROR, speed =', spd)
-        else:
-                print('Command Error! Cannot recognize command: ', data)
+        """
 
 tcpSerSock.close()
